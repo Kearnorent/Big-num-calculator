faa85bad8fdab084ea85e69912fc72597f384dd2
Charles DUCHAMP <charles.duchamp@epita.fr>
2020-07-10 16:09:45 +0200

    Parenthesis precedence working.

--- src/parser/parsing.cc
+++ src/parser/parsing.cc
@@ -4,6 +4,27 @@
 
 namespace parser
 {
+    std::vector<lexer::t_token> get_tokens_between_pars(std::vector<lexer::t_token>& tokens, size_t ind, size_t& shift)
+    {
+        int count_op = 1;
+        int count_cl = 0;
+        size_t i = ind;
+        std::vector<lexer::t_token> res;
+        while (i < tokens.size())
+        {
+            if (tokens[i].get_type() == LEFT_PAR)
+                count_op += 1;
+            else if (tokens[i].get_type() == RIGHT_PAR)
+                count_cl += 1;
+            if (count_cl == count_op)
+                break;
+            res.push_back(tokens[i]);
+            i++;
+        }
+        shift = i;
+        return res;
+    }
+
     std::shared_ptr<t_ast> parse (std::vector<lexer::t_token>& tokens, std::string& error_msg)
     {
         std::vector<operator_type> operator_stack;
@@ -12,6 +33,18 @@ namespace parser
         tok_type state = NONE;
         for (size_t i = 0; i < len_tokens; ++i)
         {
+            if (tokens[i].get_type() == LEFT_PAR)
+            {
+                size_t shift = 1;
+                std::vector<lexer::t_token> tokens_between_pars = get_tokens_between_pars(tokens, i + 1, shift);
+                std::shared_ptr<t_ast> par_ast = parse(tokens_between_pars, error_msg);
+                operand_stack.push_back(par_ast);
+                i = shift;
+                state = NUMBER;
+                continue;
+            }
+            if (tokens[i].get_type() == RIGHT_PAR)
+                continue;
             /// Parsing error cases
             if (i == len_tokens - 1 and tokens[i].get_type() != NUMBER)
             {
@@ -42,7 +75,8 @@ namespace parser
         int prio = 3;
         while (prio > 0)
         {
-            if (prio == 3) {
+            if (prio == 3)
+            {
                 int i = operator_stack.size() - 1;
                 while (i >= 0)
                 {
--- src/parser/parsing.hh
+++ src/parser/parsing.hh
@@ -11,4 +11,6 @@
 namespace parser
 {
     std::shared_ptr<t_ast> parse (std::vector<lexer::t_token>& tokens, std::string& error_msg);
+
+    std::vector<lexer::t_token> get_tokens_between_pars(std::vector<lexer::t_token>& tokens, size_t ind, size_t& shift);
 }
